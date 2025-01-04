bb98cee7ca0ffcf7bee7ba3d267cdf4a69f9543d
dhruvakhariaa <vakhariadhruv526@gmail.com>
2025-01-04 18:49:19 +0530

    Connected to the database

---
+++ src/DemoJDBC.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+
+public class DemoJDBC {
+    public static void main(String[] args) {
+        /*
+        * Import Package
+        * Load the driver and register
+        * Create connection
+        * Create statement
+        * Execute statements
+        * Process the results
+        * Close
+        * */
+
+        try{
+            Class.forName("org.postgresql.Driver"); //Optional
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:postgresql://localhost:5000/FirstDB";
+        String uname = "postgres";  //Default username
+        String pass = "Dheeraj#1234";
+        //The postgresql does not directly take case-sensitive words, that's why use such a syntax
+        String query = "SELECT * FROM \"FirstTable\"";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, uname, pass);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getString("Name") + " " + rs.getString("Age"));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        System.out.println( "Connection successfully closed");
+
+    }
+}
