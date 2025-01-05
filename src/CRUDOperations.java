06d03dfa280a893851dd056a43bee693c5875b3e
dhruvakhariaa <vakhariadhruv526@gmail.com>
2025-01-05 19:56:44 +0530

    Update and delete functions are added

--- src/CRUDOperations.java
+++ src/CRUDOperations.java
@@ -1,3 +1,5 @@
+//Genuine tip from author : Always try to keep your table name and column name in small letters
+
 import java.sql.*;
 public class CRUDOperations {
     public static void main(String[] args) {
@@ -6,11 +8,15 @@ public class CRUDOperations {
         String user = "postgres";
         String password = "Dheeraj#1234";
         String create = "insert into \"FirstTable\" values (5, 'Rajam', 24)";
+        String update = "update \"FirstTable\" set \"Name\" = 'Raja' where \"Id\" = 5";
+        String delete = "delete from \"FirstTable\" where \"Id\" = 5";
 
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
             Statement st = conn.createStatement();
-            System.out.println(st.execute(create)); //Print false when insert or update query is being executed
+//            st.execute(create); //Print false when insert or update query is being executed
+//            st.execute(update);
+            st.execute(delete);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
